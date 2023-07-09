0abcb0f983b274d8aad2bbbe3a7b92ca7eaede14
Rani Saini <sainirani1797@gmail.com>
2023-07-09 08:30:20 +0530

    ClaimSchema Updated

--- models/claimsSchema.js
+++ models/claimsSchema.js
@@ -2,41 +2,53 @@ import mongoose from "mongoose";
 
 const claimsSchema = new mongoose.Schema({
 
-    MedicalClaims: {
-
-        sickness: {
-
+    claims: {
+        "type": "object",
+        "properties": {
+            "claimNumber": {
+                "type": "string"
+            },
+            "policyNumber": {
+                "type": "string"
+            },
+            "insuredPerson": {
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string"
+                    },
+                    "dateOfBirth": {
+                        "type": "string",
+                        "format": "date"
+                    },
+                    "gender": {
+                        "type": "string",
+                        "enum": ["male", "female", "other"]
+                    }
+                },
+                "required": ["name", "dateOfBirth"]
+            },
+            "incidentDate": {
+                "type": "string",
+                "format": "date"
+            },
+            "incidentDescription": {
+                "type": "string"
+            },
+            "claimAmount": {
+                "type": "number",
+                "minimum": 0
+            },
+            "documents": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                }
+            },
         },
-        injury: {
-
-        },
-        hospitalisation: {
-
-        },
-        accidentalDeath: {
-
-        }
-
-    },
-
-    TravelInconvenience: {
-
-        MissingOfLuggage: {
-
-        },
-        MissingPassport: {
-
-        },
-        delayInPlaneArrival: {
-
-        },
-        tripCurtailment: {
-
-        },
-        loseOfMoney: {
-
-        }
+        "required": ["claimNumber", "policyNumber", "insuredPerson", "incidentDate", "incidentDescription", "claimAmount"]
     }
+
 }
 )
 
@@ -44,3 +56,40 @@ const claimsSchema = new mongoose.Schema({
 const Claims = mongoose.model("Claims", claimsSchema);
 
 export default Claims;
+
+
+// MedicalClaims: {
+
+//     sickness: {
+
+//     },
+//     injury: {
+
+//     },
+//     hospitalisation: {
+
+//     },
+//     accidentalDeath: {
+
+//     }
+
+// },
+
+// TravelInconvenience: {
+
+//     MissingOfLuggage: {
+
+//     },
+//     MissingPassport: {
+
+//     },
+//     delayInPlaneArrival: {
+
+//     },
+//     tripCurtailment: {
+
+//     },
+//     loseOfMoney: {
+
+//     }
+// }
